73c1d95309aba22f4b2e0cd346608dcb293498fd
Aaron Murphy <42772225-aarongeorgemurp@users.noreply.replit.com>
2025-06-15 17:16:43 +0000

    Fix next.config.js - remove invalid options

--- next.config.js
+++ next.config.js
@@ -7,14 +7,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  swcMinify: false,
-  experimental: {
-    esmExternals: false,
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    return config;
-  },
 }
 
 module.exports = nextConfig
